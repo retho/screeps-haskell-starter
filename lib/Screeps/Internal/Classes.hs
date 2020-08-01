8ff00815bea411bfe2a20b493bf945f51136411d
Safonov Evgeny <safonov.e.s@gmail.com>
2020-08-01 15:56:48 +0300

    -

--- lib/Screeps/Internal/Classes.hs
+++ lib/Screeps/Internal/Classes.hs
@@ -78,7 +78,7 @@ class HasRoomPosition a => IsRoomObject a where
   asRoomObject :: a -> RoomObject
   fromRoomObject :: RoomObject -> Maybe a
 
-class (IsRoomObject a, HasScreepsId a, HasStore a, HasName a, HasOwner a, Attackable a, Transferable a, Withdrawable a, NotifyWhenAttacked a) => IsSharedCreep a where
+class (IsRoomObject a, HasScreepsId a, HasStore a, HasName a, HasOwner a, Attackable a, Transferable a, NotifyWhenAttacked a) => IsSharedCreep a where
   asSharedCreep :: a -> SharedCreep
   fromSharedCreep :: SharedCreep -> Maybe a
 
@@ -99,8 +99,28 @@ instance HasScreepsId PowerCreep
 instance HasScreepsId Resource
 instance HasScreepsId Structure
 instance HasScreepsId OwnedStructure
+-- *
+instance HasScreepsId StructureContainer
 instance HasScreepsId StructureController
+instance HasScreepsId StructureExtension
+instance HasScreepsId StructureExtractor
+instance HasScreepsId StructureFactory
+instance HasScreepsId StructureInvaderCore
+instance HasScreepsId StructureKeeperLair
+instance HasScreepsId StructureLab
+instance HasScreepsId StructureLink
+instance HasScreepsId StructureNuker
+instance HasScreepsId StructureObserver
+instance HasScreepsId StructurePowerBank
+instance HasScreepsId StructurePowerSpawn
+instance HasScreepsId StructurePortal
+instance HasScreepsId StructureRampart
+instance HasScreepsId StructureRoad
 instance HasScreepsId StructureSpawn
+instance HasScreepsId StructureStorage
+instance HasScreepsId StructureTerminal
+instance HasScreepsId StructureTower
+instance HasScreepsId StructureWall
 
 
 instance HasName Room
@@ -115,43 +135,134 @@ instance HasOwner SharedCreep
 instance HasOwner Creep
 instance HasOwner PowerCreep
 instance HasOwner OwnedStructure
+-- *
 instance HasOwner StructureController
+instance HasOwner StructureExtension
+instance HasOwner StructureExtractor
+instance HasOwner StructureFactory
+instance HasOwner StructureInvaderCore
+instance HasOwner StructureKeeperLair
+instance HasOwner StructureLab
+instance HasOwner StructureLink
+instance HasOwner StructureNuker
+instance HasOwner StructureObserver
+instance HasOwner StructurePowerSpawn
+instance HasOwner StructureRampart
 instance HasOwner StructureSpawn
+instance HasOwner StructureStorage
+instance HasOwner StructureTerminal
+instance HasOwner StructureTower
 
 
 instance HasStore Store where store = id
 instance HasStore SharedCreep
 instance HasStore Creep
 instance HasStore PowerCreep
+-- *
+instance HasStore StructureContainer
+instance HasStore StructureExtension
+instance HasStore StructureFactory
+instance HasStore StructureLab
+instance HasStore StructureLink
+instance HasStore StructureNuker
+instance HasStore StructurePowerSpawn
 instance HasStore StructureSpawn
+instance HasStore StructureStorage
+instance HasStore StructureTerminal
+instance HasStore StructureTower
 
 
-instance Attackable StructureController
 instance Attackable SharedCreep
 instance Attackable Creep
 instance Attackable PowerCreep
 instance Attackable Structure
 instance Attackable OwnedStructure
+-- *
+instance Attackable StructureContainer
+instance Attackable StructureController
+instance Attackable StructureExtension
+instance Attackable StructureExtractor
+instance Attackable StructureFactory
+instance Attackable StructureInvaderCore
+instance Attackable StructureKeeperLair
+instance Attackable StructureLab
+instance Attackable StructureLink
+instance Attackable StructureNuker
+instance Attackable StructureObserver
+instance Attackable StructurePowerBank
+instance Attackable StructurePowerSpawn
+instance Attackable StructurePortal
+instance Attackable StructureRampart
+instance Attackable StructureRoad
 instance Attackable StructureSpawn
+instance Attackable StructureStorage
+instance Attackable StructureTerminal
+instance Attackable StructureTower
+instance Attackable StructureWall
 
 
 instance Transferable SharedCreep
 instance Transferable Creep
 instance Transferable PowerCreep
+instance Transferable Structure
+-- *
+instance Transferable StructureContainer
+instance Transferable StructureExtension
+instance Transferable StructureFactory
+instance Transferable StructureLab
+instance Transferable StructureLink
+instance Transferable StructureNuker
+instance Transferable StructurePowerSpawn
+instance Transferable StructureSpawn
+instance Transferable StructureStorage
+instance Transferable StructureTerminal
+instance Transferable StructureTower
 
 
-instance Withdrawable SharedCreep
-instance Withdrawable Creep
-instance Withdrawable PowerCreep
+instance Withdrawable Tombstone
+instance Withdrawable Ruin
+instance Withdrawable Structure
+-- *
+instance Withdrawable StructureContainer
+instance Withdrawable StructureExtension
+instance Withdrawable StructureFactory
+instance Withdrawable StructureLab
+instance Withdrawable StructureLink
+instance Withdrawable StructureNuker
+instance Withdrawable StructurePowerSpawn
+instance Withdrawable StructureSpawn
+instance Withdrawable StructureStorage
+instance Withdrawable StructureTerminal
+instance Withdrawable StructureTower
 
 
-instance NotifyWhenAttacked StructureController
 instance NotifyWhenAttacked SharedCreep
 instance NotifyWhenAttacked Creep
 instance NotifyWhenAttacked PowerCreep
 instance NotifyWhenAttacked Structure
 instance NotifyWhenAttacked OwnedStructure
+-- *
+instance NotifyWhenAttacked StructureContainer
+instance NotifyWhenAttacked StructureController
+instance NotifyWhenAttacked StructureExtension
+instance NotifyWhenAttacked StructureExtractor
+instance NotifyWhenAttacked StructureFactory
+instance NotifyWhenAttacked StructureInvaderCore
+instance NotifyWhenAttacked StructureKeeperLair
+instance NotifyWhenAttacked StructureLab
+instance NotifyWhenAttacked StructureLink
+instance NotifyWhenAttacked StructureNuker
+instance NotifyWhenAttacked StructureObserver
+instance NotifyWhenAttacked StructurePowerBank
+instance NotifyWhenAttacked StructurePowerSpawn
+instance NotifyWhenAttacked StructurePortal
+instance NotifyWhenAttacked StructureRampart
+instance NotifyWhenAttacked StructureRoad
 instance NotifyWhenAttacked StructureSpawn
+instance NotifyWhenAttacked StructureStorage
+instance NotifyWhenAttacked StructureTerminal
+instance NotifyWhenAttacked StructureTower
+instance NotifyWhenAttacked StructureWall
 
 
 instance Harvestable Source
@@ -175,8 +286,28 @@ instance HasRoomPosition Ruin
 instance HasRoomPosition Tombstone
 instance HasRoomPosition Structure
 instance HasRoomPosition OwnedStructure
+-- *
+instance HasRoomPosition StructureContainer
 instance HasRoomPosition StructureController
+instance HasRoomPosition StructureExtension
+instance HasRoomPosition StructureExtractor
+instance HasRoomPosition StructureFactory
+instance HasRoomPosition StructureInvaderCore
+instance HasRoomPosition StructureKeeperLair
+instance HasRoomPosition StructureLab
+instance HasRoomPosition StructureLink
+instance HasRoomPosition StructureNuker
+instance HasRoomPosition StructureObserver
+instance HasRoomPosition StructurePowerBank
+instance HasRoomPosition StructurePowerSpawn
+instance HasRoomPosition StructurePortal
+instance HasRoomPosition StructureRampart
+instance HasRoomPosition StructureRoad
 instance HasRoomPosition StructureSpawn
+instance HasRoomPosition StructureStorage
+instance HasRoomPosition StructureTerminal
+instance HasRoomPosition StructureTower
+instance HasRoomPosition StructureWall
 
 
 instance IsSharedCreep SharedCreep where
@@ -235,12 +366,70 @@ instance IsRoomObject Structure where
 instance IsRoomObject OwnedStructure where
   asRoomObject = coerce
   fromRoomObject = fromJSRef . maybe_owned_structure . toJSRef
+-- *
+instance IsRoomObject StructureContainer where
+  asRoomObject = coerce
+  fromRoomObject = fromJSRef . maybe_structure_container . toJSRef
 instance IsRoomObject StructureController where
   asRoomObject = coerce
   fromRoomObject = fromJSRef . maybe_structure_controller . toJSRef
+instance IsRoomObject StructureExtension where
+  asRoomObject = coerce
+  fromRoomObject = fromJSRef . maybe_structure_extension . toJSRef
+instance IsRoomObject StructureExtractor where
+  asRoomObject = coerce
+  fromRoomObject = fromJSRef . maybe_structure_extractor . toJSRef
+instance IsRoomObject StructureFactory where
+  asRoomObject = coerce
+  fromRoomObject = fromJSRef . maybe_structure_factory . toJSRef
+instance IsRoomObject StructureInvaderCore where
+  asRoomObject = coerce
+  fromRoomObject = fromJSRef . maybe_structure_invader_core . toJSRef
+instance IsRoomObject StructureKeeperLair where
+  asRoomObject = coerce
+  fromRoomObject = fromJSRef . maybe_structure_keeper_lair . toJSRef
+instance IsRoomObject StructureLab where
+  asRoomObject = coerce
+  fromRoomObject = fromJSRef . maybe_structure_lab . toJSRef
+instance IsRoomObject StructureLink where
+  asRoomObject = coerce
+  fromRoomObject = fromJSRef . maybe_structure_link . toJSRef
+instance IsRoomObject StructureNuker where
+  asRoomObject = coerce
+  fromRoomObject = fromJSRef . maybe_structure_nuker . toJSRef
+instance IsRoomObject StructureObserver where
+  asRoomObject = coerce
+  fromRoomObject = fromJSRef . maybe_structure_observer . toJSRef
+instance IsRoomObject StructurePowerBank where
+  asRoomObject = coerce
+  fromRoomObject = fromJSRef . maybe_structure_power_bank . toJSRef
+instance IsRoomObject StructurePowerSpawn where
+  asRoomObject = coerce
+  fromRoomObject = fromJSRef . maybe_structure_power_spawn . toJSRef
+instance IsRoomObject StructurePortal where
+  asRoomObject = coerce
+  fromRoomObject = fromJSRef . maybe_structure_portal . toJSRef
+instance IsRoomObject StructureRampart where
+  asRoomObject = coerce
+  fromRoomObject = fromJSRef . maybe_structure_rampart . toJSRef
+instance IsRoomObject StructureRoad where
+  asRoomObject = coerce
+  fromRoomObject = fromJSRef . maybe_structure_road . toJSRef
 instance IsRoomObject StructureSpawn where
   asRoomObject = coerce
   fromRoomObject = fromJSRef . maybe_structure_spawn . toJSRef
+instance IsRoomObject StructureStorage where
+  asRoomObject = coerce
+  fromRoomObject = fromJSRef . maybe_structure_storage . toJSRef
+instance IsRoomObject StructureTerminal where
+  asRoomObject = coerce
+  fromRoomObject = fromJSRef . maybe_structure_terminal . toJSRef
+instance IsRoomObject StructureTower where
+  asRoomObject = coerce
+  fromRoomObject = fromJSRef . maybe_structure_tower . toJSRef
+instance IsRoomObject StructureWall where
+  asRoomObject = coerce
+  fromRoomObject = fromJSRef . maybe_structure_wall . toJSRef
 
 
 instance IsStructure Structure where
@@ -249,23 +438,124 @@ instance IsStructure Structure where
 instance IsStructure OwnedStructure where
   asStructure = coerce
   fromStructure = fromJSRef . maybe_owned_structure . toJSRef
+-- *
+instance IsStructure StructureContainer where
+  asStructure = coerce
+  fromStructure = fromJSRef . maybe_structure_container . toJSRef
 instance IsStructure StructureController where
   asStructure = coerce
   fromStructure = fromJSRef . maybe_structure_controller . toJSRef
+instance IsStructure StructureExtension where
+  asStructure = coerce
+  fromStructure = fromJSRef . maybe_structure_extension . toJSRef
+instance IsStructure StructureExtractor where
+  asStructure = coerce
+  fromStructure = fromJSRef . maybe_structure_extractor . toJSRef
+instance IsStructure StructureFactory where
+  asStructure = coerce
+  fromStructure = fromJSRef . maybe_structure_factory . toJSRef
+instance IsStructure StructureInvaderCore where
+  asStructure = coerce
+  fromStructure = fromJSRef . maybe_structure_invader_core . toJSRef
+instance IsStructure StructureKeeperLair where
+  asStructure = coerce
+  fromStructure = fromJSRef . maybe_structure_keeper_lair . toJSRef
+instance IsStructure StructureLab where
+  asStructure = coerce
+  fromStructure = fromJSRef . maybe_structure_lab . toJSRef
+instance IsStructure StructureLink where
+  asStructure = coerce
+  fromStructure = fromJSRef . maybe_structure_link . toJSRef
+instance IsStructure StructureNuker where
+  asStructure = coerce
+  fromStructure = fromJSRef . maybe_structure_nuker . toJSRef
+instance IsStructure StructureObserver where
+  asStructure = coerce
+  fromStructure = fromJSRef . maybe_structure_observer . toJSRef
+instance IsStructure StructurePowerBank where
+  asStructure = coerce
+  fromStructure = fromJSRef . maybe_structure_power_bank . toJSRef
+instance IsStructure StructurePowerSpawn where
+  asStructure = coerce
+  fromStructure = fromJSRef . maybe_structure_power_spawn . toJSRef
+instance IsStructure StructurePortal where
+  asStructure = coerce
+  fromStructure = fromJSRef . maybe_structure_portal . toJSRef
+instance IsStructure StructureRampart where
+  asStructure = coerce
+  fromStructure = fromJSRef . maybe_structure_rampart . toJSRef
+instance IsStructure StructureRoad where
+  asStructure = coerce
+  fromStructure = fromJSRef . maybe_structure_road . toJSRef
 instance IsStructure StructureSpawn where
   asStructure = coerce
   fromStructure = fromJSRef . maybe_structure_spawn . toJSRef
+instance IsStructure StructureStorage where
+  asStructure = coerce
+  fromStructure = fromJSRef . maybe_structure_storage . toJSRef
+instance IsStructure StructureTerminal where
+  asStructure = coerce
+  fromStructure = fromJSRef . maybe_structure_terminal . toJSRef
+instance IsStructure StructureTower where
+  asStructure = coerce
+  fromStructure = fromJSRef . maybe_structure_tower . toJSRef
+instance IsStructure StructureWall where
+  asStructure = coerce
+  fromStructure = fromJSRef . maybe_structure_wall . toJSRef
 
 
 instance IsOwnedStructure OwnedStructure where
   asOwnedStructure = id
   fromOwnedStruture = pure
+-- *
 instance IsOwnedStructure StructureController where
   asOwnedStructure = coerce
   fromOwnedStruture = fromJSRef . maybe_structure_controller . toJSRef
+instance IsOwnedStructure StructureExtension where
+  asOwnedStructure = coerce
+  fromOwnedStruture = fromJSRef . maybe_structure_extension . toJSRef
+instance IsOwnedStructure StructureExtractor where
+  asOwnedStructure = coerce
+  fromOwnedStruture = fromJSRef . maybe_structure_extractor . toJSRef
+instance IsOwnedStructure StructureFactory where
+  asOwnedStructure = coerce
+  fromOwnedStruture = fromJSRef . maybe_structure_factory . toJSRef
+instance IsOwnedStructure StructureInvaderCore where
+  asOwnedStructure = coerce
+  fromOwnedStruture = fromJSRef . maybe_structure_invader_core . toJSRef
+instance IsOwnedStructure StructureKeeperLair where
+  asOwnedStructure = coerce
+  fromOwnedStruture = fromJSRef . maybe_structure_keeper_lair . toJSRef
+instance IsOwnedStructure StructureLab where
+  asOwnedStructure = coerce
+  fromOwnedStruture = fromJSRef . maybe_structure_lab . toJSRef
+instance IsOwnedStructure StructureLink where
+  asOwnedStructure = coerce
+  fromOwnedStruture = fromJSRef . maybe_structure_link . toJSRef
+instance IsOwnedStructure StructureNuker where
+  asOwnedStructure = coerce
+  fromOwnedStruture = fromJSRef . maybe_structure_nuker . toJSRef
+instance IsOwnedStructure StructureObserver where
+  asOwnedStructure = coerce
+  fromOwnedStruture = fromJSRef . maybe_structure_observer . toJSRef
+instance IsOwnedStructure StructurePowerSpawn where
+  asOwnedStructure = coerce
+  fromOwnedStruture = fromJSRef . maybe_structure_power_spawn . toJSRef
+instance IsOwnedStructure StructureRampart where
+  asOwnedStructure = coerce
+  fromOwnedStruture = fromJSRef . maybe_structure_rampart . toJSRef
 instance IsOwnedStructure StructureSpawn where
   asOwnedStructure = coerce
   fromOwnedStruture = fromJSRef . maybe_structure_spawn . toJSRef
+instance IsOwnedStructure StructureStorage where
+  asOwnedStructure = coerce
+  fromOwnedStruture = fromJSRef . maybe_structure_storage . toJSRef
+instance IsOwnedStructure StructureTerminal where
+  asOwnedStructure = coerce
+  fromOwnedStruture = fromJSRef . maybe_structure_terminal . toJSRef
+instance IsOwnedStructure StructureTower where
+  asOwnedStructure = coerce
+  fromOwnedStruture = fromJSRef . maybe_structure_tower . toJSRef
 
 
 -- *
@@ -294,5 +584,25 @@ foreign import javascript "$1 instanceof Ruin ? $1 : null" maybe_ruin :: JSVal -
 foreign import javascript "$1 instanceof Tombstone ? $1 : null" maybe_tombstone :: JSVal -> JSVal
 foreign import javascript "$1 instanceof Structure ? $1 : null" maybe_structure :: JSVal -> JSVal
 foreign import javascript "$1 instanceof OwnedStructure ? $1 : null" maybe_owned_structure :: JSVal -> JSVal
+-- *
+foreign import javascript "$1 instanceof StructureContainer ? $1 : null" maybe_structure_container :: JSVal -> JSVal
 foreign import javascript "$1 instanceof StructureController ? $1 : null" maybe_structure_controller :: JSVal -> JSVal
+foreign import javascript "$1 instanceof StructureExtension ? $1 : null" maybe_structure_extension :: JSVal -> JSVal
+foreign import javascript "$1 instanceof StructureExtractor ? $1 : null" maybe_structure_extractor :: JSVal -> JSVal
+foreign import javascript "$1 instanceof StructureFactory ? $1 : null" maybe_structure_factory :: JSVal -> JSVal
+foreign import javascript "$1 instanceof StructureInvaderCore ? $1 : null" maybe_structure_invader_core :: JSVal -> JSVal
+foreign import javascript "$1 instanceof StructureKeeperLair ? $1 : null" maybe_structure_keeper_lair :: JSVal -> JSVal
+foreign import javascript "$1 instanceof StructureLab ? $1 : null" maybe_structure_lab :: JSVal -> JSVal
+foreign import javascript "$1 instanceof StructureLink ? $1 : null" maybe_structure_link :: JSVal -> JSVal
+foreign import javascript "$1 instanceof StructureNuker ? $1 : null" maybe_structure_nuker :: JSVal -> JSVal
+foreign import javascript "$1 instanceof StructureObserver ? $1 : null" maybe_structure_observer :: JSVal -> JSVal
+foreign import javascript "$1 instanceof StructurePowerBank ? $1 : null" maybe_structure_power_bank :: JSVal -> JSVal
+foreign import javascript "$1 instanceof StructurePowerSpawn ? $1 : null" maybe_structure_power_spawn :: JSVal -> JSVal
+foreign import javascript "$1 instanceof StructurePortal ? $1 : null" maybe_structure_portal :: JSVal -> JSVal
+foreign import javascript "$1 instanceof StructureRampart ? $1 : null" maybe_structure_rampart :: JSVal -> JSVal
+foreign import javascript "$1 instanceof StructureRoad ? $1 : null" maybe_structure_road :: JSVal -> JSVal
 foreign import javascript "$1 instanceof StructureSpawn ? $1 : null" maybe_structure_spawn :: JSVal -> JSVal
+foreign import javascript "$1 instanceof StructureStorage ? $1 : null" maybe_structure_storage :: JSVal -> JSVal
+foreign import javascript "$1 instanceof StructureTerminal ? $1 : null" maybe_structure_terminal :: JSVal -> JSVal
+foreign import javascript "$1 instanceof StructureTower ? $1 : null" maybe_structure_tower :: JSVal -> JSVal
+foreign import javascript "$1 instanceof StructureWall ? $1 : null" maybe_structure_wall :: JSVal -> JSVal
