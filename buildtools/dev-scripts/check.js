b1c3fbf755862b53d8c6df3cd71978ab841dad69
Safonov Evgeny <safonov.e.s@gmail.com>
2020-09-23 10:38:58 +0300

    -

--- buildtools/dev-scripts/check.js
+++ buildtools/dev-scripts/check.js
@@ -4,11 +4,11 @@ const { execSync } = require('child_process')
 const run = (cmd) => execSync(cmd, {stdio: [process.stdin, process.stdout, process.stderr]})
 
 const args = argsParser(process.argv);
-const profile = args['profile'];
+const subname = args['sub'];
 const from_scratch = args['from-scratch'];
 
 const builddir_base = '.cabal-screeps-work/ahc-cabal-build';
-const builddir = profile ? `${builddir_base}-${profile}` : builddir_base
+const builddir = subname ? `${builddir_base}-${subname}` : builddir_base
 
 from_scratch && run(`rm -rf ${builddir}`)
 run(`ahc-cabal new-build main -O0 --builddir=${builddir}`)
